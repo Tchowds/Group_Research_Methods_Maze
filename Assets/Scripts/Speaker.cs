377ad8544fcb757b34530d2d9a50a954c6ab0a1e
Tchowds <zcabtac@ucl.ac.uk-use-Github-Anonymous>
2024-03-16 15:10:29 +0000

    save ubiq

--- Assets/Scripts/Speaker.cs
+++ Assets/Scripts/Speaker.cs
@@ -8,6 +8,7 @@ using Ubiq.Messaging;
 public class Speaker : MonoBehaviour
 {
     public AvatarManager avatarManager;
+    public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
@@ -19,27 +20,44 @@ public class Speaker : MonoBehaviour
     void Update()
     {
         if (avatarManager != null) {
-            Transform head;
-            foreach (Avatar avatar in avatarManager.Avatars) {
-                if (avatar != avatarManager.LocalAvatar) {
-                    head = avatar.transform.Find("Body/Floating_Head");
-                    Debug.Log("Head position: " + head);
-                    gameObject.transform.position = head.position;
-
-
-                    //Calculate number of objects in between both players
-                    //Access position of local avatar
-                    Transform localHead = avatarManager.LocalAvatar.transform.Find("Body/Floating_Head");
-                    // Find vector in which the remote player is in
-                    Vector3 direction = head.position - localHead.position;
-                    //Distance between each player
-                    float distance = direction.magnitude;
-                    //Gets a list of all the objects between both players with .Length field to use
-                    RaycastHit[] hits = Physics.RaycastAll(localHead.position, direction, distance);
-                    Debug.Log(hits.Length);
-
+            List<Avatar> list = new List<Avatar>(avatarManager.Avatars);
+            if(list.Count > 1){
+                Transform localHead = avatarManager.LocalAvatar.transform.Find("Body/Floating_Head");
+                Transform remoteHead;
+                foreach(Avatar avatar in avatarManager.Avatars){
+                    if(avatar != avatarManager.LocalAvatar){
+                        remoteHead = avatar.transform.Find("Body/Floating_Head");
+                        break;
+                    }
                 }
+                gameObject.transform.position = localHead.position;
+                audioSource.volume = 0.5f;
+                
+
             }
+
+
+            // Transform head;
+            // foreach (Avatar avatar in avatarManager.Avatars) {
+            //     if (avatar != avatarManager.LocalAvatar) {
+            //         head = avatar.transform.Find("Body/Floating_Head");
+            //         Debug.Log("Head position: " + head);
+            //         gameObject.transform.position = head.position;
+
+
+            //         //Calculate number of objects in between both players
+            //         //Access position of local avatar
+            //         Transform localHead = avatarManager.LocalAvatar.transform.Find("Body/Floating_Head");
+            //         // Find vector in which the remote player is in
+            //         Vector3 direction = head.position - localHead.position;
+            //         //Distance between each player
+            //         float distance = direction.magnitude;
+            //         //Gets a list of all the objects between both players with .Length field to use
+            //         RaycastHit[] hits = Physics.RaycastAll(localHead.position, direction, distance);
+            //         Debug.Log(hits.Length);
+
+            //     }
+            // }
         } else {
             Debug.Log("avatar manager not found");
         }
