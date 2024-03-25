51eb6541aac212745a2bbb7bea9b7dbc67035b06
Tchowds <zcabtac@ucl.ac.uk>
2024-03-25 12:39:32 +0000

    log plotter and teleport fixed

--- Assets/Scripts/trajectoryLog.cs
+++ Assets/Scripts/trajectoryLog.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using Ubiq.Avatars;
 using Avatar = Ubiq.Avatars.Avatar;
+using UnityEngine.SceneManagement;
 
 public class trajectoryLog : MonoBehaviour
 {
@@ -16,31 +17,44 @@ public class trajectoryLog : MonoBehaviour
     public float writeInterval = 1f;
 
     private float timeElapsed = 0f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         TeleportSetup teleport = transform.parent.GetComponent<TeleportSetup>();
         avatarManager = transform.parent.GetComponentInChildren<AvatarManager>();
-        
-        if(avatarManager == null){
+
+        if (avatarManager == null)
+        {
             Debug.Log("ERROR: avatar manager not found");
         }
-            
-        if(teleport == null){
+
+        if (teleport == null)
+        {
             Debug.Log("ERROR: teleport script not found");
         }
-            
 
-        string dateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-        string fileName = ("player-" + teleport.playerNum) + ".txt";
-        string remoteName = ("remote-" + teleport.playerNum) + ".txt";
+        int localNum = teleport.playerNum;
+        int remoteNum;
+        if (localNum == 1)
+        {
+            remoteNum = 2;
+        }
+        else
+        {
+            remoteNum = 1;
+        }
+
+        string dateTime = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss_");
+        string mode = SceneManager.GetActiveScene().name;
+        string fileName = dateTime + ("player-" + localNum) + mode + ".txt";
+        string remoteName = dateTime + ("remote-" + remoteNum) + mode + ".txt";
         filePath = Path.Combine(folderPath, fileName);
         remotePath = Path.Combine(folderPath, remoteName);
         WriteToFile(filePath, fileName, false);
         WriteToFile(remotePath, remoteName, false);
 
-        
+
 
     }
 
@@ -48,37 +62,60 @@ public class trajectoryLog : MonoBehaviour
     void Update()
     {
         List<Avatar> list = new List<Avatar>(avatarManager.Avatars);
-        if(list.Count > 1){
+        Transform localHead = avatarManager.LocalAvatar.transform.Find("Body/Floating_Head");
+        if(list.Count == 2)
+        {
             timeElapsed += Time.deltaTime;
-
             if(timeElapsed >= writeInterval){
                 timeElapsed = 0f;
 
+                Transform remoteHead = null;
                 foreach(Avatar avatar in avatarManager.Avatars){
-                    if(avatar == avatarManager.LocalAvatar){
-                        Transform posHead = avatarManager.LocalAvatar.transform.Find("Body/Floating_Head");
-                        string toWrite = posHead.position.x + "," + posHead.position.z + "," + posHead.rotation.eulerAngles.y;
-                        WriteToFile(filePath, toWrite);
-
-                    } else{
-                        Transform posHead = avatar.transform.Find("Body/Floating_Head");
-                        string toWrite = posHead.position.x + "," + posHead.position.z + "," + posHead.rotation.eulerAngles.y;
-                        WriteToFile(remotePath, toWrite);
+                    if(avatar != avatarManager.LocalAvatar){
+                        remoteHead = avatar.transform.Find("Body/Floating_Head");
+                        break;
                     }
                 }
+
+                Vector3 direction = remoteHead.position - localHead.position;
+                float distance = direction.magnitude;
+                RaycastHit[] hits = Physics.RaycastAll(localHead.position, direction, distance);
+
+                string localWrite = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + "," + localHead.position.x + "," + localHead.position.z + "," + localHead.rotation.eulerAngles.y + "," + hits.Length;
+                string remoteWrite = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + "," + remoteHead.position.x + "," + remoteHead.position.z + "," + remoteHead.rotation.eulerAngles.y + "," + hits.Length;
+
+                WriteToFile(filePath, localWrite);
+                WriteToFile(remotePath, remoteWrite);
             }
-        }
+        } 
+        // else if (list.Count == 1)
+        // {
+        //     timeElapsed += Time.deltaTime;
+
+        //     if (timeElapsed >= writeInterval)
+        //     {
+        //         timeElapsed = 0f;
+        //         string toWrite = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + "," + localHead.position.x + "," + localHead.position.z + "," + localHead.rotation.eulerAngles.y;
+        //         WriteToFile(filePath, toWrite);
+        //     }
+        // }
 
     }
 
 
-    private void WriteToFile(string path, string content, bool append = true){
+    private void WriteToFile(string path, string content, bool append = true)
+    {
         StreamWriter writer = new StreamWriter(path, append);
-        try{
+        try
+        {
             writer.WriteLine(content);
-        } catch (Exception e){
+        }
+        catch (Exception e)
+        {
             Debug.Log("Error writing to file: " + e.Message);
-        } finally{
+        }
+        finally
+        {
             writer.Close();
         }
     }
